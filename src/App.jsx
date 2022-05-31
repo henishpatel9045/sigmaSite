542a39c5dac2a10d814780da261db5c412b4a3bc
Henish Patel <200120107182@git.org.in>
2022-05-31 17:04:46 +0530

    header section completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./app/components/header/Header";
 import Navbar from "./app/components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   );
 }
---
+++ src/app/components/header/Header.jsx
@@ -0,0 +1,59 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import "./header.css";
+import BG1 from "../../assets/header-1.png";
+
+function Header() {
+  return (
+    <>
+      <Box
+        id="home"
+        component={"section"}
+        sx={{ flexDirection: { xs: "column", md: "row" } }}
+      >
+        <Box className="header-content">
+          <Typography variant="h3" gutterBottom className="header-title">
+            Lorem ipsum dolor amet
+          </Typography>
+          <Typography variant="h5" gutterBottom className="header-description">
+            Nam sollicitud nunc, cursus eros vulputate sed. Vestibulum lobortis.
+          </Typography>
+        </Box>
+        <Box className="header-img">
+          <img src={BG1} alt="Header-1" />
+        </Box>
+      </Box>
+      <Box
+        sx={{ columnGap: { xs: ".6rem", md: "2rem" } }}
+        className="header-links"
+      >
+        <Box>
+          <Typography variant="h6" className="link">
+            Business Solution
+          </Typography>
+          <Typography variant="body2">
+            Interdum et malesuada ac ante…
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" className="link">
+            Business Solution
+          </Typography>
+          <Typography variant="body2">
+            Interdum et malesuada ac ante…
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" className="link">
+            Business Solution
+          </Typography>
+          <Typography variant="body2">
+            Interdum et malesuada ac ante…
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default Header;
